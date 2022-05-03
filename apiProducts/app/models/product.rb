e309304db978a28f440fa904d8f411629bc2e96c
mellyssaStephanny <mellyssah.mendes@live.com>
2022-05-03 09:16:14 -0300

    configure els indexing

--- apiProducts/app/models/product.rb
+++ apiProducts/app/models/product.rb
@@ -1,5 +1,16 @@
 class Product
   include Mongoid::Document
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  index_name Rails.application.class.parent_name.underscore
+  document_type self.name.downcase
+
+  settings index: { number_of_shards: 1 } do
+    mapping dynamic: false do
+      indexes :name, analyzer: 'portuguese'
+      indexes :description, analyzer: 'portuguese'
+
   field :sku, type: String
   field :name, type: String
   field :price, type: Float
@@ -16,5 +27,21 @@ class Product
     hash = super()
     hash['id'] = hash.delete('_id').to_s
     hash.as_json(only: %w[id sku name price amount description ])
+  end
+
+  def as_indexed_json(options = nil)
+    self.as_json( only: [ :name, :description ] )
+  end
+
+  def self.search(query)
+    __elasticsearch__.search(
+    {
+      query: {
+         multi_match: {
+           query: query,
+           fields: ['name', 'description']
+         }
+       },
+    }
   end
 end
