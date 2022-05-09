2f3b74fba2c07f46fb30c3ea358d8260679fcfd3
mellyssaStephanny <mellyssah.mendes@live.com>
2022-05-09 10:36:14 -0300

    add barcode and validate sku, price and barcode fields

--- apiProducts/app/models/product.rb
+++ apiProducts/app/models/product.rb
@@ -5,16 +5,20 @@ class Product
   field :price, type: Float
   field :amount, type: Integer
   field :description, type: String
+  field :barcode, type: Integer
 
-  validates :sku, presence: true
+  validates :sku, presence: true, format: { with: /\A[a-zA-Z-]*\z/, message: "The SKU field must contain only alphanumerics and hyphen" }
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, numericality: {:greater_than: 0 }
   validates :amount, presence: true
   validates :description, presence: true
+  validates :barcode, length: { in: 8..13 }
+
+  
   
   def as_json(*args)
     hash = super()
     hash['id'] = hash.delete('_id').to_s
-    hash.as_json(only: %w[id sku name price amount description ])
+    hash.as_json(only: %w[id sku name price amount barcode description ])
   end
 end
