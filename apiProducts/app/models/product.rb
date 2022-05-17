4be22b5be1666d68aac87589fe308dac8820e787
mellyssaStephanny <mellyssah.mendes@live.com>
2022-05-17 10:04:39 -0300

    update product.rb

--- apiProducts/app/models/product.rb
+++ apiProducts/app/models/product.rb
@@ -1,24 +1,24 @@
 class Product
   include Mongoid::Document
+
   field :sku, type: String
   field :name, type: String
   field :price, type: Float
+  field :barcode, type: String
   field :amount, type: Integer
   field :description, type: String
-  field :barcode, type: Integer
 
-  validates :sku, presence: true, format: { with: /\A[a-zA-Z-]*\z/, message: "The SKU field must contain only alphanumerics and hyphen" }
+  validates :sku, presence: true
   validates :name, presence: true
-  validates :price, numericality: { greater_than: 0 }
+  validates :price, presence: true
+  validates :barcode, presence: true
   validates :amount, presence: true
   validates :description, presence: true
-  validates :barcode, length: { in: 8..13 }
-
-  
   
   def as_json(*args)
     hash = super()
     hash['id'] = hash.delete('_id').to_s
-    hash.as_json(only: %w[id sku name price amount barcode description ])
+    hash.as_json(only: %w[id sku name price barcode amount description ])
   end
+
 end
