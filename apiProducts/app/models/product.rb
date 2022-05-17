ddf37b51f0a449f6f9e5af89f8644f4dc067aeb2
mellyssaStephanny <mellyssah.mendes@live.com>
2022-05-17 10:08:27 -0300

    add validations again

--- apiProducts/app/models/product.rb
+++ apiProducts/app/models/product.rb
@@ -8,12 +8,12 @@ class Product
   field :amount, type: Integer
   field :description, type: String
 
-  validates :sku, presence: true
+  validates :sku, presence: true, format: { with: /\A[a-zA-Z-]*\z/, message: "The SKU field must contain only alphanumerics and hyphen" 
   validates :name, presence: true
-  validates :price, presence: true
-  validates :barcode, presence: true
+  validates :price, numericality: {:greater_than: 0 }
   validates :amount, presence: true
   validates :description, presence: true
+  validates :barcode, length: { in: 8..13 }
   
   def as_json(*args)
     hash = super()
